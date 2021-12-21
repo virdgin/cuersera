6ab12cc88ad6924723d03fb94e83ba1cb55492c3
virdgin <am.klepik@yandex.ru>
2021-12-21 18:02:47 +0300

    first com

---
+++ buses3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <set>
+using namespace std;
+int main(){
+    int Q;
+    cin>>Q;
+    map<int,set<string>> buses;
+    for (int i=0; i<Q;i++){
+        int number;
+        cin>>number;
+        for (int j=0;j<number;j++){
+          set<string> stops; 
+        string stop;
+        }
+    }
+    return 0;
+}
