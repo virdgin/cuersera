1e64054b3cf074bb90db23a840738a0071a88841
virdgin <am.klepik@yandex.ru>
2021-12-21 19:15:28 +0300

    relize

--- buses3.cpp
+++ buses3.cpp
@@ -6,14 +6,21 @@ using namespace std;
 int main(){
     int Q;
     cin>>Q;
-    map<int,set<string>> buses;
+    int number;
+    map<set<string>, int> buses;
     for (int i=0; i<Q;i++){
-        int number;
         cin>>number;
-        for (int j=0;j<number;j++){
-          set<string> stops; 
-        string stop;
+        set<string> stops;
+         string stop;
+        for (int j=0;j<number;j++){        
+        cin>>stop;
+        stops.insert(stop);
         }
-    }
+        if (buses.count(stops)==0){
+            buses[stops]=buses.size()+1;
+            cout <<"New bus "<<buses[stops]<<endl;
+        }
+        else cout <<"Already exists for " << buses[stops]<<endl;
+        }
     return 0;
 }
