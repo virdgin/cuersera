4308a2894a6be43cc1ac5a132a68d7daf3d13b90
virdgin <am.klepik@yandex.ru>
2022-01-18 15:27:37 +0300

    home work

---
+++ solution.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include<set>
+
+using namespace std;
+class Data
+{
+public:
+    Data(int new_year, int new_month, int new_day){
+        year=new_year;
+        month=new_month;
+        day=new_day;
+    }
+
+    int Year()const{
+        return year;
+    }
+    int Month() const{
+        return month;
+    }
+    int Day() const{
+        return day;
+    }
+private:
+    int year;
+    int month;
+    int day;
+};
+
+class DataBase
+{
+
+private:
+    map<Data,set<string>> database;
+};
+int main()
+{
+string event;
+
+    return 0;
+}
